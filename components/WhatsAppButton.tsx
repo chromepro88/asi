65525682feae44d00930cdb1f59f42f1d29b721a
chromepro88 <chromepro88@gmail.com>
2025-03-05 18:16:34 +0800

    whatsapp message

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export function WhatsAppButton() {
   return (
     <a
-      href="https://wa.me/6588379368"
+      href="https://wa.me/6588379368https://wa.me/6588379368?text=Hi%2C%20I%20want%20to%20find%20out%20more%20about%20AI%20Landline%20%40%20%2499%2FMonth.%20Can%20you%20please%20share%20more%20details%20about%20the%20features%20and%20how%20it%20works%3F"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50 transition-all hover:scale-110 [transform-origin:center] animate-float-prominent"
