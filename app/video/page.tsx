cab3f6d8fa7eda165dfcd1d4ee7266bbf60764de
chromepro88 <chromepro88@gmail.com>
2025-03-08 12:27:26 +0800

    change to youtube

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation"
 
 export default function VideoPage() {
-  redirect("https://vimeo.com/1063173744/923e83f492?ts=0&share=copy")
+  redirect("https://youtu.be/XUSQW6PLcpY?si=Dpk20Z_8a24wS_3V")
 }
 
