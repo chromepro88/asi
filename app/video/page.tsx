b560d9dca027ffde05cdcf316c37b9c7ddc702f9
chromepro88 <chromepro88@gmail.com>
2025-03-08 19:08:18 +0800

    redirect to channel

--- app/video/page.tsx
+++ app/video/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation"
 
 export default function VideoPage() {
-  redirect("https://youtu.be/XUSQW6PLcpY?si=Dpk20Z_8a24wS_3V")
+  redirect("https://www.youtube.com/@AISuperSG")
 }
 
