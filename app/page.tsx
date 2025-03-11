0dc4a5daae32dbbcb7038ad7d1246a2598699b51
chromepro88 <chromepro88@gmail.com>
2025-03-11 17:25:17 +0800

    youtube video

--- app/page.tsx
+++ app/page.tsx
@@ -100,7 +100,7 @@ export default function Home() {
           <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/YpdkDWfu8Ms?si=-InQ66sHEm37V0xd"
+            src="https://www.youtube.com/embed/esN1EH3AOWg?si=iQNgyqydG97xVEvH"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
