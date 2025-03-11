c35193d4bfc2b93b8a50b9038c3df19d2d150aa5
chromepro88 <chromepro88@gmail.com>
2025-03-11 14:48:06 +0800

    logging

--- app/page.tsx
+++ app/page.tsx
@@ -58,7 +58,7 @@ export default function Home() {
           <div className="grid gap-10 sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
             <div className="flex flex-col items-center space-y-4 text-center">
               <Phone className="h-12 w-12 text-[#062e86]" />
-              <h3 className="text-2xl font-bold">Whatsapp Log</h3>
+              <h3 className="text-2xl font-bold">Whatsapp Logging</h3>
               <p className="text-gray-300">
                 Get Whatsapp full transcript of call to your number.
               </p>
