2e84361d60ef49fcd9426fe7f92a175f2c14009d
chromepro88 <chromepro88@gmail.com>
2025-03-12 21:52:30 +0800

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import Script from 'next/script'; // Import Script from next/script
 import './globals.css';
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: 'ASI',
