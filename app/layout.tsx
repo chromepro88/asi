a20a0ac5496f5e4b9e7f97fc1406cb73f6e9cb93
chromepro88 <chromepro88@gmail.com>
2025-03-12 21:31:06 +0800

    google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Head from 'next/head'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -13,19 +14,23 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <head>
+    <>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
         <link rel="icon" href="logo.png" type="image/x-icon" />
+        {/* Google tag (gtag.js) */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-16922868318"></script>
         <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'AW-16922868318');
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-16922868318');
+          `}
         </script>
-      </head>
+      </Head>
       <body>{children}</body>
-    </html>
+    </>
   )
 }
