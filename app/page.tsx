d6f20bfd298d94a43f7a633637015f0985f72d7f
chromepro88 <chromepro88@gmail.com>
2025-03-14 09:08:52 +0800

    whatsapp chatbot

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,7 @@ export default function Home() {
           <div className="flex flex-col items-center space-y-4 text-center">
             <div className="space-y-2">
               <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                AI Whatsapp Chatbot
+                Whatsapp Chatbot
               </h1>
               <p className="mx-auto max-w-[700px] text-gray-300 md:text-xl">
                 AI-empowered Closing Sales
